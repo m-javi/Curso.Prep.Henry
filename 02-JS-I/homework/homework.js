8f7c7da7d6e2f173757a8a746846d12898509e26
mbjavi <mbjavi.02@gmail.com>
2021-07-22 22:18:08 -0300

    finish

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -243,7 +243,7 @@ function esVocal(letra){
   if(letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u'){
     return 'Es vocal';
   }
-  return 'Dato incorrecto';
+  return 'Dato incorreco';
 }
 
 
